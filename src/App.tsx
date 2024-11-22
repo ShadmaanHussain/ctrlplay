172a38b8554da71715adf3dcfa193485fe11672b
Shadmaan Hussain <shdhussain1299@gmail.com>
2024-11-22 12:05:46 +0530

    feat: Initialized light and dark theme using shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import './App.css'
-import { Button } from '@/components/ui/button'
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/dark-mode/theme-provider";
+import { ModeToggle } from "./components/dark-mode/mode-toggle";
 
 function App() {
-
   return (
     <>
-      <h2 className='text-5xl'>Heading 1</h2>
-      <Button>Click Me</Button>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Taxing Laughter: The Joke Tax Chronicles
+        </h1>
+        <Button>Click Me</Button>
+        <ModeToggle />
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
