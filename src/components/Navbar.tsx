22308bf83217f5224f7aa6025b31f1d93cb28e04
Shadmaan Hussain <shdhussain1299@gmail.com>
2024-12-03 22:17:41 +0530

    fix: navbar styled and adjusted

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
 import { ModeToggle } from "@/components/dark-mode/mode-toggle";
 
 function Navbar() {
   return (
-    <nav className="py-4 px-8 border-b-gray-100 bg-[var(--navbar-background)]">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-2xl font-bold">CTRL<span className='text-red-600'>PLAY</span></div>
-        <ul className="flex items-center space-x-6">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Contact</a></li>
-          <li><ModeToggle/></li>
+    <nav className="py-4 px-8 border-b-gray-100 bg-background">
+      <div className="container mx-auto flex items-center">
+        <div className="text-2xl font-bold mr-10">
+          CTRL<span className="text-red-600">PLAY</span>
+        </div>
+        <ul className="flex items-center w-full">
+          <li className="mr-5">
+            <a href="#">Discover</a>
+          </li>
+          <li className="mr-5">
+            <a href="#">Browse</a>
+          </li>
+          <li className="mr-5">
+            <a href="#">News</a>
+          </li>
+          <li className="ml-auto">
+            <ModeToggle />
+          </li>
         </ul>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
