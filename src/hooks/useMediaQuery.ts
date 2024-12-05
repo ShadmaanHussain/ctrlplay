8c7b70dc01a775a4174c12d9c58e1571c744beb7
Shadmaan Hussain <shdhussain1299@gmail.com>
2024-12-05 17:38:44 +0530

    feat: navbar support for mobile and useMediaQuery hook

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,19 @@
+import { throttle } from "lodash";
+import { useEffect, useState } from "react";
+
+function useMediaQuery(threshold: number) {
+  const [isMobileView, setIsMobileView] = useState<boolean>(
+    window.innerWidth < threshold
+  );
+
+  useEffect(() => {
+    const handleResize = throttle(() => {
+      setIsMobileView(window.innerWidth < threshold);
+    }, 200);
+    window.addEventListener("resize", handleResize);
+  }, [threshold]);
+
+  return isMobileView;
+}
+
+export default useMediaQuery;
