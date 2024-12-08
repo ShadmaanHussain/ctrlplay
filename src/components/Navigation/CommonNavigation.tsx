c1aa84353e38ebc63eb7ea307011259ec9eaceda
Shadmaan Hussain <shdhussain1299@gmail.com>
2024-12-08 13:21:46 +0530

    fix: navbar buttons highlighted when we are on that page

--- src/components/Navigation/CommonNavigation.tsx
+++ src/components/Navigation/CommonNavigation.tsx
@@ -5,29 +5,34 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 function CommonNavigation() {
+  const location = useLocation();
+  console.log(location);
+
+  const isActive = (path : string) => location.pathname === path;
+
   return (
     <NavigationMenu className="mx-auto mt-8 sm:mx-0 sm:mt-0">
       <NavigationMenuList  className="flex-col gap-6 sm:flex-row sm:gap-0">
         <NavigationMenuItem>
           <Link to="/discover">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <NavigationMenuLink className={`${navigationMenuTriggerStyle()} ${isActive("/discover") ? "bg-accent" : ""}`}>
               Discover
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <Link to="/browse">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <NavigationMenuLink className={`${navigationMenuTriggerStyle()} ${isActive("/browse") ? "bg-accent" : ""}`}>
               Browse
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <Link to="/news">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <NavigationMenuLink className={`${navigationMenuTriggerStyle()} ${isActive("/news") ? "bg-accent" : ""}`}>
               News
             </NavigationMenuLink>
           </Link>
