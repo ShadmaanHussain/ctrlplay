2c7076e635a2aaed63fcf1aeefba6ff29bd60c52
Shadmaan Hussain <shdhussain1299@gmail.com>
2025-03-02 14:12:26 +0530

    chore: constants cleanup

--- src/lib/RawgUtils.ts
+++ src/lib/RawgUtils.ts
@@ -1,12 +1,10 @@
 import {
-  RAWG_API_KEY,
-  RAWG_BASE_URL,
-  RAWG_GAMES_ENDPOINT,
+  FETCH_GAMES_ENDPOINT,
 } from "@/constants/RawgConstants";
 
 export const fetchGames = async (page: number) => {
   const response = await fetch(
-    `${RAWG_BASE_URL}${RAWG_GAMES_ENDPOINT}?key=${RAWG_API_KEY}&page=${page}`
+    `${FETCH_GAMES_ENDPOINT}&page=${page}`
   );
   if (!response.ok) {
     throw new Error("Network response was not ok");
